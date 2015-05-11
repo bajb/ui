d56294db262bd2a9870b4fd7d942df56231bb7e7
Brooke Bryan <brooke@bajb.net>
2015-05-11 14:18:50 +0100

    Foundation UI

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
 # Fortifi UI Components
+
+Panel
+- Container only
+- Bordered
+- Titled
+ - With Actions
+
+PanelHeader
+- Colour BG
+- Icon (Left)
+- Action Icons (Right)
+- with tags
+
+Bars
+- Progress Bars
+- Star Pickers (1 out of 5)
+- Ascii start bar
+
+Buttons
+- Primary Button
+- Seconary Button
+- Standard Button
+- Danger Button
+- Warning Button
+
+- With icons
+- flat buttons
+
+Dropdown Menu
+
+Activity Feed
+- Simple Story
+- Detailed Story
+
+Avatar Icon
+
+Forms
+- Label / Inputs
+
+Hovercards
+- Title
+- Seconard title (Status)
+- Buttons Bar
+
+Stats Panel
+
+Weekly Graphs
+
+Sortable Table
+
+Dropdown Text
+- Set Default
+- Add Items
+
+Dropdown Text & Panel
+
+Headers
+- Title
+- Navigation Items
+- Set Right content
+
+List Header
+- Action Buttons
+- Filter Form
+
+Datanode Header
+- Action Buttons
+- Tags & Flags
+
+Lists
+ - Buttons
+ - Title
+ - Header?
+ 
+ Tags
+
+Toggle Button
+
+Filterable table
--- src/Ui.php
+++ src/Ui.php
@@ -5,61 +5,81 @@ use Packaged\Dispatch\AssetManager;
 
 final class UI
 {
+  /**
+   * Floating Alignment
+   */
   const CENTER_BLOCK = 'center-block';
   const FLOAT_LEFT = 'pull-left';
   const FLOAT_RIGHT = 'pull-right';
   const CLEARFIX = 'clearfix';
 
+  const CLEAR_FLOAT_BOTH = 'f-clr-both';
+  const CLEAR_FLOAT_LEFT = 'f-clr-left';
+  const CLEAR_FLOAT_RIGHT = 'f-clr-right';
+  const CLEAR_FLOAT_NONE = 'f-clr-none';
+
+  /**
+   * Visibility
+   */
+  const INVISIBLE = 'invisible';
   const HIDE = 'hidden';
   const HIDE_TEXT = 'text-hide';
   const SHOW = 'show';
 
-  const MARGIN_SMALL = 'ms';
-  const MARGIN_MEDIUM = 'mm';
-  const MARGIN_LARGE = 'ml';
+  /**
+   * Margins
+   */
 
-  const MARGIN_SMALL_LEFT = 'msl';
-  const MARGIN_MEDIUM_LEFT = 'mml';
-  const MARGIN_LARGE_LEFT = 'mll';
+  const MARGIN_SMALL = 'f-ms';
+  const MARGIN_MEDIUM = 'f-mm';
+  const MARGIN_LARGE = 'f-ml';
+  const MARGIN_SMALL_LEFT = 'f-msl';
+  const MARGIN_MEDIUM_LEFT = 'f-mml';
+  const MARGIN_LARGE_LEFT = 'f-mll';
+  const MARGIN_SMALL_RIGHT = 'f-msr';
+  const MARGIN_MEDIUM_RIGHT = 'f-mmr';
+  const MARGIN_LARGE_RIGHT = 'f-mlr';
+  const MARGIN_SMALL_BOTTOM = 'f-msb';
+  const MARGIN_MEDIUM_BOTTOM = 'f-mmb';
+  const MARGIN_LARGE_BOTTOM = 'f-mlb';
+  const MARGIN_SMALL_TOP = 'f-mst';
+  const MARGIN_MEDIUM_TOP = 'f-mmt';
+  const MARGIN_LARGE_TOP = 'f-mlt';
 
-  const MARGIN_SMALL_RIGHT = 'msr';
-  const MARGIN_MEDIUM_RIGHT = 'mmr';
-  const MARGIN_LARGE_RIGHT = 'mlr';
+  /**
+   * Padding
+   */
 
-  const MARGIN_SMALL_BOTTOM = 'msb';
-  const MARGIN_MEDIUM_BOTTOM = 'mmb';
-  const MARGIN_LARGE_BOTTOM = 'mlb';
+  const PADDING_SMALL = 'f-ps';
+  const PADDING_MEDIUM = 'f-pm';
+  const PADDING_LARGE = 'f-pl';
+  const PADDING_SMALL_LEFT = 'f-psl';
+  const PADDING_MEDIUM_LEFT = 'f-pml';
+  const PADDING_LARGE_LEFT = 'f-pll';
+  const PADDING_SMALL_RIGHT = 'f-psr';
+  const PADDING_MEDIUM_RIGHT = 'f-pmr';
+  const PADDING_LARGE_RIGHT = 'f-plr';
+  const PADDING_SMALL_BOTTOM = 'f-psb';
+  const PADDING_MEDIUM_BOTTOM = 'f-pmb';
+  const PADDING_LARGE_BOTTOM = 'f-plb';
+  const PADDING_SMALL_TOP = 'f-pst';
+  const PADDING_MEDIUM_TOP = 'f-pmt';
+  const PADDING_LARGE_TOP = 'f-plt';
 
-  const MARGIN_SMALL_TOP = 'mst';
-  const MARGIN_MEDIUM_TOP = 'mmt';
-  const MARGIN_LARGE_TOP = 'mlt';
+  /**
+   * Text Styles
+   */
 
-  const PADDING_SMALL = 'ps';
-  const PADDING_MEDIUM = 'pm';
-  const PADDING_LARGE = 'pl';
-
-  const PADDING_SMALL_LEFT = 'psl';
-  const PADDING_MEDIUM_LEFT = 'pml';
-  const PADDING_LARGE_LEFT = 'pll';
-
-  const PADDING_SMALL_RIGHT = 'psr';
-  const PADDING_MEDIUM_RIGHT = 'pmr';
-  const PADDING_LARGE_RIGHT = 'plr';
-
-  const PADDING_SMALL_BOTTOM = 'psb';
-  const PADDING_MEDIUM_BOTTOM = 'pmb';
-  const PADDING_LARGE_BOTTOM = 'plb';
-
-  const PADDING_SMALL_TOP = 'pst';
-  const PADDING_MEDIUM_TOP = 'pmt';
-  const PADDING_LARGE_TOP = 'plt';
-
-  const TEXT_UPPERCASE = 'text-uppercase';
-  const TEXT_STRIKE = 'text-strike';
+  const TEXT_UPPERCASE = 'f-txt-uppercase';
+  const TEXT_STRIKE = 'f-txt-strike';
 
   const TEXT_CENTER = 'text-center';
   const TEXT_RIGHT = 'text-right';
   const TEXT_LEFT = 'text-left';
+
+  /**
+   * State Based Colours
+   */
 
   const TEXT_DEFAULT = 'text-default';
   const TEXT_SUCCESS = 'text-success';
@@ -83,17 +103,56 @@ final class UI
   const LABEL_DANGER = 'label-danger';
   const LABEL_PRIMARY = 'label-primary';
 
-  const TABLE_FIXED_LEFT = 'first';
-  const TABLE_FIXED_RIGHT = 'last';
+  /**
+   * Specific Colours
+   */
 
-  const CLEAR_FLOAT_BOTH = 'clear-both';
-  const CLEAR_FLOAT_LEFT = 'clear-left';
-  const CLEAR_FLOAT_RIGHT = 'clear-right';
-  const CLEAR_FLOAT_NONE = 'clear-none';
+  const TEXT_RED = 'f-red';
+  const BG_RED = 'f-bg-red';
+  const TEXT_LIGHT_RED = 'f-l-red';
+  const BG_LIGHT_RED = 'f-bg-l-red';
+  const TEXT_ORANGE = 'f-orange';
+  const BG_ORANGE = 'f-bg-orange';
+  const TEXT_LIGHT_ORANGE = 'f-l-orange';
+  const BG_LIGHT_ORANGE = 'f-bg-l-orange';
+  const TEXT_YELLOW = 'f-yellow';
+  const BG_YELLOW = 'f-bg-yellow';
+  const TEXT_LIGHT_YELLOW = 'f-l-yellow';
+  const BG_LIGHT_YELLOW = 'f-bg-l-yellow';
+  const TEXT_GREEN = 'f-green';
+  const BG_GREEN = 'f-bg-green';
+  const TEXT_LIGHT_GREEN = 'f-l-green';
+  const BG_LIGHT_GREEN = 'f-bg-l-green';
+  const TEXT_BLUE = 'f-blue';
+  const BG_BLUE = 'f-bg-blue';
+  const TEXT_LIGHT_BLUE = 'f-l-blue';
+  const BG_LIGHT_BLUE = 'f-bg-l-blue';
+  const TEXT_SKY = 'f-sky';
+  const BG_SKY = 'f-bg-sky';
+  const TEXT_LIGHT_SKY = 'f-l-sky';
+  const BG_LIGHT_SKY = 'f-bg-l-sky';
+  const TEXT_INDIGO = 'f-indigo';
+  const BG_INDIGO = 'f-bg-indigo';
+  const TEXT_LIGHT_INDIGO = 'f-l-indigo';
+  const BG_LIGHT_INDIGO = 'f-bg-l-indigo';
+  const TEXT_PINK = 'f-pink';
+  const BG_PINK = 'f-bg-pink';
+  const TEXT_LIGHT_PINK = 'f-l-pink';
+  const BG_LIGHT_PINK = 'f-bg-l-pink';
 
   public static function boot()
   {
     $am = AssetManager::vendorType('fortifi', 'ui');
+
+    //Require Base UI
     $am->requireCss('assets/css/ui-base');
+    $am->requireJs('assets/css/ui-base');
+
+    //Require JQuery
+    $am->requireJs('vendor_assets/jquery/2.1.4.min');
+
+    //Require Bootstrap
+    $am->requireCss('vendor_assets/bootstrap/3.3.4.min');
+    $am->requireJs('vendor_assets/bootstrap/3.3.4.min');
   }
 }
