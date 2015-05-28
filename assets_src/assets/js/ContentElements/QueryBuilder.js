cd3e0029fa370ffcbe0725e0b89fe5b414a3c760
Tom Kay <oridan82@gmail.com>
2015-05-28 11:37:37 +0100

    improve object model of QueryBuilder

--- assets_src/assets/js/ContentElements/QueryBuilder.js
+++ assets_src/assets/js/ContentElements/QueryBuilder.js
@@ -5,13 +5,20 @@
 (function ($)
 {
   /**
-   * @typedef {{}} QueryBuilderDefinition
+   * @typedef {Object} QueryBuilderDefinition
    * @property {String} display
    * @property {String} comparators
    * @property {String} values
    * @property {String} ajaxUrl
    * @property {String} dataType
    * @property {String} required
+   */
+
+  /**
+   * @typedef {Object} QueryBuilderRule
+   * @property {String} key
+   * @property {String} comparator
+   * @property {String} value
    */
 
   var defaultOptions = {
@@ -121,319 +128,42 @@
     this._rules = [];
   }
 
-  QueryBuilder.prototype.init = function (options)
+  QueryBuilder.prototype = (function ()
   {
-    var $ele = $(this._ele);
-    options = $.extend({}, defaultOptions, $ele.data(), options);
-    this.options(options);
-    if (this._options && 'definitions' in this._options && this._options.definitions)
-    {
-      this.definitions(this._options.definitions);
-    }
-    if (this._options && 'rules' in this._options && this._options.rules)
-    {
-      this.rules(this._options.rules);
-    }
-  };
+    var types = {
+      'eq':  {string: 'text', number: 'number', date: 'date'},
+      'neq': {string: 'text', number: 'number', date: 'date'},
+      'in':  {string: 'token', number: 'token'},
+      'nin': {string: 'token', number: 'token'},
+      'gt':  {number: 'number', date: 'date', decimal: 'decimal'},
+      'gte': {number: 'number', date: 'date', decimal: 'decimal'},
+      'lt':  {number: 'number', date: 'date', decimal: 'decimal'},
+      'lte': {number: 'number', date: 'date', decimal: 'decimal'},
+      'bet': {number: 'number', date: 'date', decimal: 'decimal'},
+      'age': {date: 'age'}
+    };
+    var typeNames = {
+      'eq':  'Equals',
+      'neq': 'Does Not Equal',
+      'in':  'In',
+      'nin': 'Not In',
+      'gt':  'Greater Than',
+      'gte': 'Greater Than or Equal to',
+      'lt':  'Less Than',
+      'lte': 'Less Than or Equal to',
+      'bet': 'Between',
+      'age': 'was'
+    };
 
-  QueryBuilder.prototype.options = function (data)
-  {
-    if (data === undefined)
-    {
-      return this._options;
-    }
-    this._options = data;
-    this.redraw();
-  };
-
-  QueryBuilder.prototype.definitions = function (data)
-  {
-    var self = this;
-    if (data === undefined)
-    {
-      return this._definitions;
-    }
-    else if (typeof data === 'string')
-    {
-      $.getJSON(
-        data, {}, function (defs)
-        {
-          self._definitions = defs;
-          self.redraw();
-        }
-      );
-      return;
-    }
-    this._definitions = data;
-    this.redraw();
-  };
-
-  QueryBuilder.prototype.rules = function (data)
-  {
-    var self = this;
-    if (data === undefined)
-    {
-      // no data - return object of all rules
-      var currentData = [];
-      $('.qb-rules .qb-rule', $(this._ele)).each(
-        function ()
-        {
-          var key = $('.qb-key', this).val();
-          if (key)
-          {
-            currentData.push(
-              {
-                key:        key,
-                comparator: $('.qb-comparator', this).val()
-                              ? $('.qb-comparator', this).val() : 'eq',
-                value:      $('.qb-value', this).val()
-              }
-            );
-          }
-        }
-      );
-      return currentData;
-    }
-    // if data is object, read it into rules
-    else if (typeof data === 'object')
-    {
-      var rules = [];
-      if (data)
-      {
-        $.each(
-          data, function (key)
-          {
-            if (typeof this == 'object')
-            {
-              if (this instanceof String)
-              {
-                rules.push({key: key, comparator: 'eq', value: this});
-              }
-              else
-              {
-                if ('key' in this && 'comparator' in this && 'value' in this)
-                {
-                  rules.push(this);
-                }
-                else
-                {
-                  rules.push({key: key, comparator: 'in', value: this});
-                }
-              }
-            }
-            else
-            {
-              rules.push({key: key, comparator: 'eq', value: this});
-            }
-          }
-        );
-      }
-      this._rules = rules;
-      this.redraw();
-      return;
-    }
-    else if (typeof data === 'function')
-    {
-      this._rules = data();
-      this.redraw();
-      return;
-    }
-    else if (typeof data === 'string')
-    {
-      $.getJSON(
-        data, {}, function (rules)
-        {
-          self._rules = rules;
-          self.redraw();
-        }
-      );
-      return;
-    }
-    throw 'Unknown data format for rules';
-  };
-
-  QueryBuilder.prototype.redraw = function ()
-  {
-    var self = this;
-    if (!$(this._ele).hasClass('qb-container'))
-    {
-      $(this._ele).addClass('qb-container')
-        .html($('<div class="qb-rules"/>'))
-        .append($('<button class="qb-button qb-add-rule">+</button>'));
-    }
-    else
-    {
-      $('.qb-rules', this._ele).empty();
-    }
-    if (self._definitions)
-    {
-      // add rules
-      if (self._rules.length)
-      {
-        $.each(
-          self._rules, function ()
-          {
-            self.addRule(this);
-          }
-        );
-        $(self._ele).trigger('init.querybuilder');
-      }
-      // add any required fields which are not already added
-      $.each(
-        self._definitions, function (defKey)
-        {
-          if (this.required)
-          {
-            var found = false;
-            $.each(
-              self._rules, function ()
-              {
-                if (this.key == defKey)
-                {
-                  found = true;
-                }
-              }
-            );
-            if (!found)
-            {
-              self.addRule({key: defKey});
-            }
-          }
-        }
-      );
-
-      // if no rules, add an empty one
-      if (!$('.qb-rules .qb-rule', this._ele).length)
-      {
-        self.addRule();
-      }
-
-      if (this._definitions.length)
-      {
-        var count = 0;
-        $.each(
-          this._definitions, function ()
-          {
-            if (!this.required)
-            {
-              count++;
-            }
-          }
-        );
-        if (!count)
-        {
-          $('.qb-add-rule').hide();
-        }
-      }
-    }
-  };
-
-  var types = {
-    'eq':  {string: 'text', number: 'number', date: 'date'},
-    'neq': {string: 'text', number: 'number', date: 'date'},
-    'in':  {string: 'token', number: 'token'},
-    'nin': {string: 'token', number: 'token'},
-    'gt':  {number: 'number', date: 'date', decimal: 'decimal'},
-    'gte': {number: 'number', date: 'date', decimal: 'decimal'},
-    'lt':  {number: 'number', date: 'date', decimal: 'decimal'},
-    'lte': {number: 'number', date: 'date', decimal: 'decimal'},
-    'bet': {number: 'number', date: 'date', decimal: 'decimal'},
-    'age': {date: 'age'}
-  };
-  var typeNames = {
-    'eq':  'Equals',
-    'neq': 'Does Not Equal',
-    'in':  'In',
-    'nin': 'Not In',
-    'gt':  'Greater Than',
-    'gte': 'Greater Than or Equal to',
-    'lt':  'Less Than',
-    'lte': 'Less Than or Equal to',
-    'bet': 'Between',
-    'age': 'was'
-  };
-
-  QueryBuilder.prototype.removeRule = function (idx)
-  {
-    $('.qb-rules .qb-rule', this._ele).get(idx).remove();
-    if (!$('.qb-rule', this._ele).length)
-    {
-      this.addRule();
-    }
-  };
-
-  QueryBuilder.prototype.addRule = function (ruleData, idx)
-  {
-    var $row = $('<div class="qb-rule"/>'),
-      $propertySel = $('<select class="qb-key"/>').appendTo($row),
-      ruleKey = ruleData ? ruleData.key : null;
     /**
-     * @type {QueryBuilderDefinition}
+     * Get an input for a specified rule/definition
+     *
+     * @param {QueryBuilderRule} ruleData
+     * @param {QueryBuilderDefinition} definition
+     * @returns {jQuery}
+     * @private
      */
-    var definition = ruleKey ? this._definitions[ruleKey] : null;
-
-    if (!ruleKey)
-    {
-      $propertySel.append('<option value=""> - SELECT -</option>');
-    }
-    if (ruleKey && !definition)
-    {
-      return;
-    }
-    $.each(
-      this._definitions, function (optionKey)
-      {
-        if (this.required && ruleKey !== optionKey)
-        {
-          return;
-        }
-        var selected = (ruleKey == optionKey) ? ' selected="selected"' : '';
-        $propertySel.append('<option' + selected + ' value="' + optionKey + '">' + this.display + '</option>');
-      }
-    );
-
-    if (definition)
-    {
-      if (definition.required)
-      {
-        $propertySel.attr('disabled', 'disabled');
-      }
-      if (!definition.dataType)
-      {
-        definition.dataType = 'string';
-      }
-      if (definition['comparators'])
-      {
-        var $comparatorSel = $('<select class="qb-comparator"/>').appendTo($row);
-        $.each(
-          definition['comparators'], function ()
-          {
-            if (!ruleData.comparator)
-            {
-              ruleData.comparator = this;
-            }
-            var selected = (ruleData.comparator == this) ? ' selected="selected"' : '';
-            $comparatorSel.append('<option' + selected + ' value="' + this + '">' + typeNames[this] + '</option>');
-          }
-        );
-      }
-      getInput().appendTo($row);
-    }
-    if (definition && !definition.required)
-    {
-      $('<button class="qb-button qb-remove-rule">x</button>').appendTo($row);
-    }
-
-    if (idx !== undefined)
-    {
-      $('.qb-rule:eq(' + idx + ')', this._ele).replaceWith($row);
-    }
-    else
-    {
-      $row.appendTo($('.qb-rules', this._ele));
-    }
-
-    function getInput()
+    function getInput(ruleData, definition)
     {
       var $input, inputType;
       if (!ruleData.comparator && definition.dataType == 'bool')
@@ -454,8 +184,7 @@
       }
       if (!inputType)
       {
-        console.error('Input type not found for ' + ruleData.comparator + ' ' + definition.dataType);
-        return;
+        throw 'Input type not found for ' + ruleData.comparator + ' ' + definition.dataType;
       }
       switch (inputType)
       {
@@ -483,12 +212,337 @@
           $input = $('<input type="text" value="' + (ruleData.value ? ruleData.value : '') + '"/>');
           break;
         default:
-          console.error('Input type not found for ' + inputType);
-          return;
+          throw 'Input type not found for ' + inputType;
       }
 
       $input.addClass('qb-value');
       return $input;
     }
-  };
+
+    /**
+     * Initialise with options - also the default command
+     *
+     * @param {Object} options
+     */
+    function init(options)
+    {
+      var $ele = $(this._ele);
+      options = $.extend({}, defaultOptions, $ele.data(), options);
+      this.options(options);
+      if (this._options && 'definitions' in this._options && this._options.definitions)
+      {
+        this.definitions(this._options.definitions);
+      }
+      if (this._options && 'rules' in this._options && this._options.rules)
+      {
+        this.rules(this._options.rules);
+      }
+    }
+
+    /**
+     * Set/Get options
+     *
+     * @param data
+     * @returns {*}
+     */
+    function options(data)
+    {
+      if (data === undefined)
+      {
+        return this._options;
+      }
+      this._options = data;
+      this.redraw();
+    }
+
+    /**
+     * Set/Get definitions
+     *
+     * @param data
+     * @returns {*}
+     */
+    function definitions(data)
+    {
+      var self = this;
+      if (data === undefined)
+      {
+        return this._definitions;
+      }
+      else if (typeof data === 'string')
+      {
+        $.getJSON(
+          data, {}, function (defs)
+          {
+            self._definitions = defs;
+            self.redraw();
+          }
+        );
+        return;
+      }
+      this._definitions = data;
+      this.redraw();
+    }
+
+    /**
+     * Set/Get rules
+     * Set triggers redraw
+     *
+     * @param data
+     * @returns {Array}
+     */
+    function rules(data)
+    {
+      var self = this;
+      if (data === undefined)
+      {
+        // no data - return object of all rules
+        var currentData = [];
+        $('.qb-rules .qb-rule', $(this._ele)).each(
+          function ()
+          {
+            var key = $('.qb-key', this).val();
+            if (key)
+            {
+              currentData.push(
+                {
+                  key:        key,
+                  comparator: $('.qb-comparator', this).val()
+                                ? $('.qb-comparator', this).val() : 'eq',
+                  value:      $('.qb-value', this).val()
+                }
+              );
+            }
+          }
+        );
+        return currentData;
+      }
+      // if data is object, read it into rules
+      else if (typeof data === 'object')
+      {
+        var rules = [];
+        if (data)
+        {
+          $.each(
+            data, function (key)
+            {
+              if (typeof this == 'object')
+              {
+                if (this instanceof String)
+                {
+                  rules.push({key: key, comparator: 'eq', value: this});
+                }
+                else
+                {
+                  if ('key' in this && 'comparator' in this && 'value' in this)
+                  {
+                    rules.push(this);
+                  }
+                  else
+                  {
+                    rules.push({key: key, comparator: 'in', value: this});
+                  }
+                }
+              }
+              else
+              {
+                rules.push({key: key, comparator: 'eq', value: this});
+              }
+            }
+          );
+        }
+        this._rules = rules;
+        this.redraw();
+        return null;
+      }
+      else if (typeof data === 'function')
+      {
+        this._rules = data();
+        this.redraw();
+        return null;
+      }
+      else if (typeof data === 'string')
+      {
+        $.getJSON(
+          data, {}, function (rules)
+          {
+            self._rules = rules;
+            self.redraw();
+          }
+        );
+        return null;
+      }
+      throw 'Unknown data format for rules';
+    }
+
+    /**
+     * Redraw the container
+     */
+    function redraw()
+    {
+      var self = this;
+      if (!$(this._ele).hasClass('qb-container'))
+      {
+        $(this._ele).addClass('qb-container')
+          .html($('<div class="qb-rules"/>'))
+          .append($('<button class="qb-button qb-add-rule">+</button>'));
+      }
+      else
+      {
+        $('.qb-rules', this._ele).empty();
+      }
+      if (self._definitions)
+      {
+        // add rules
+        if (self._rules.length)
+        {
+          $.each(
+            self._rules, function ()
+            {
+              self.addRule(this);
+            }
+          );
+        }
+        $(self._ele).trigger('change.querybuilder');
+        // add any required fields which are not already added
+        $.each(
+          self._definitions, function (defKey)
+          {
+            if (this.required)
+            {
+              var found = false;
+              $.each(
+                self._rules, function ()
+                {
+                  if (this.key == defKey)
+                  {
+                    found = true;
+                  }
+                }
+              );
+              if (!found)
+              {
+                self.addRule({key: defKey});
+              }
+            }
+          }
+        );
+
+        // if no rules, add an empty one
+        if (!$('.qb-rules .qb-rule', this._ele).length)
+        {
+          self.addRule();
+        }
+
+        if (this._definitions.length)
+        {
+          var count = 0;
+          $.each(
+            this._definitions, function ()
+            {
+              if (!this.required)
+              {
+                count++;
+              }
+            }
+          );
+          if (!count)
+          {
+            $('.qb-add-rule').hide();
+          }
+        }
+      }
+    }
+
+    function removeRule(idx)
+    {
+      $('.qb-rules .qb-rule', this._ele).get(idx).remove();
+      if (!$('.qb-rule', this._ele).length)
+      {
+        this.addRule();
+      }
+    }
+
+    function addRule(ruleData, idx)
+    {
+      var $row = $('<div class="qb-rule"/>'),
+        $propertySel = $('<select class="qb-key"/>').appendTo($row),
+        ruleKey = ruleData ? ruleData.key : null;
+      /**
+       * @type {QueryBuilderDefinition}
+       */
+      var definition = ruleKey ? this._definitions[ruleKey] : null;
+
+      if (!ruleKey)
+      {
+        $propertySel.append('<option value=""> - SELECT -</option>');
+      }
+      if (ruleKey && !definition)
+      {
+        return;
+      }
+      $.each(
+        this._definitions, function (optionKey)
+        {
+          if (this.required && ruleKey !== optionKey)
+          {
+            return;
+          }
+          var selected = (ruleKey == optionKey) ? ' selected="selected"' : '';
+          $propertySel.append('<option' + selected + ' value="' + optionKey + '">' + this.display + '</option>');
+        }
+      );
+
+      if (definition)
+      {
+        if (definition.required)
+        {
+          $propertySel.attr('disabled', 'disabled');
+        }
+        if (!definition.dataType)
+        {
+          definition.dataType = 'string';
+        }
+        if (definition['comparators'])
+        {
+          var $comparatorSel = $('<select class="qb-comparator"/>').appendTo($row);
+          $.each(
+            definition['comparators'], function ()
+            {
+              if (!ruleData.comparator)
+              {
+                ruleData.comparator = this;
+              }
+              var selected = (ruleData.comparator == this) ? ' selected="selected"' : '';
+              $comparatorSel.append('<option' + selected + ' value="' + this + '">' + typeNames[this] + '</option>');
+            }
+          );
+        }
+        getInput(ruleData, definition).appendTo($row);
+      }
+      if (definition && !definition.required)
+      {
+        $('<button class="qb-button qb-remove-rule">x</button>').appendTo($row);
+      }
+
+      if (idx !== undefined)
+      {
+        $('.qb-rule:eq(' + idx + ')', this._ele).replaceWith($row);
+      }
+      else
+      {
+        $row.appendTo($('.qb-rules', this._ele));
+      }
+    }
+
+    return {
+      constructor: QueryBuilder,
+      init:        init,
+      options:     options,
+      definitions: definitions,
+      rules:       rules,
+      redraw:      redraw,
+      removeRule:  removeRule,
+      addRule:     addRule
+    };
+  })();
 })(jQuery);
