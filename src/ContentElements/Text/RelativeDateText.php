704814554f0ee21abd9f42907b4d8d7b762bab7a
Jleagle <jimeagle@gmail.com>
2015-08-04 11:00:35 +0100

    Ability to get original timestamp

--- src/ContentElements/Text/RelativeDateText.php
+++ src/ContentElements/Text/RelativeDateText.php
@@ -6,20 +6,27 @@ use Packaged\Glimpse\Tags\Span;
 
 class RelativeDateText extends Span
 {
+  protected $_timestamp;
+
   public function __construct($date = null)
   {
     if(is_int($date))
     {
-      $timestamp = $date;
+      $this->_timestamp = $date;
     }
     else
     {
-      $timestamp = strtotime($date);
+      $this->_timestamp = strtotime($date);
     }
 
-    $carbon = Carbon::createFromTimestamp($timestamp);
+    $carbon = Carbon::createFromTimestamp($this->_timestamp);
     $this->setContent($carbon->diffForHumans());
-    $this->setAttribute('title', gmdate("Y-m-d H:i:s", $timestamp));
+    $this->setAttribute('title', gmdate("Y-m-d H:i:s", $this->_timestamp));
     $this->addClass('f-txt-rel-date');
+  }
+
+  public function getTimestamp()
+  {
+    return $this->_timestamp;
   }
 }
