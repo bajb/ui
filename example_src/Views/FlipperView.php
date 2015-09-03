8a24a1f7b4e202e26f15f8d7a07bf9b9868adaa7
Tom Kay <oridan82@gmail.com>
2015-09-03 13:23:26 +0100

    add support for vertical flipper

--- example_src/Views/FlipperView.php
+++ example_src/Views/FlipperView.php
@@ -7,13 +7,11 @@ use Packaged\Glimpse\Tags\Div;
 class FlipperView extends AbstractUiExampleView
 {
   /**
-   * @group Horizontal
+   * @group Flipper
    */
   final public function horizontalFlipper()
   {
     return Flipper::create(
-      '100px',
-      '100px',
       Div::create('front')->setAttribute(
         'style',
         'width:100%;height:100%;background-color:red'
@@ -21,7 +19,28 @@ class FlipperView extends AbstractUiExampleView
       Div::create('back')->setAttribute(
         'style',
         'width:100%;height:100%;background-color:black'
-      )
+      ),
+      100,
+      100
     );
+  }
+
+  /**
+   * @group Flipper
+   */
+  final public function verticalFlipper()
+  {
+    return Flipper::create(
+      Div::create('front')->setAttribute(
+        'style',
+        'width:100%;height:100%;background-color:red'
+      ),
+      Div::create('back')->setAttribute(
+        'style',
+        'width:100%;height:100%;background-color:black'
+      ),
+      100,
+      100
+    )->setVertical();
   }
 }
--- src/ContentElements/Flipper/Flipper.php
+++ src/ContentElements/Flipper/Flipper.php
@@ -11,6 +11,7 @@ class Flipper extends UiElement
   protected $_height;
   protected $_frontContent;
   protected $_backContent;
+  protected $_vertical = false;
 
   public function processIncludes(AssetManager $assetManager, $vendor = false)
   {
@@ -24,29 +25,54 @@ class Flipper extends UiElement
     }
   }
 
-  public static function create($width, $height, $front, $back)
+  public static function create(
+    $front, $back, $width = '100px', $height = '16px'
+  )
   {
     $flipper = new static;
-    $flipper->_width = $width;
-    $flipper->_height = $height;
+
     $flipper->_frontContent = $front;
     $flipper->_backContent = $back;
+
+    if(is_int($width))
+    {
+      $width = $width . 'px';
+    }
+    $flipper->_width = $width;
+
+    if(is_int($height))
+    {
+      $height = $height . 'px';
+    }
+    $flipper->_height = $height;
+
     return $flipper;
+  }
+
+  public function setVertical($vertical = true)
+  {
+    $this->_vertical = $vertical;
+    return $this;
   }
 
   protected function _produceHtml()
   {
-    return Div::create(
+    $div = Div::create(
       [
         Div::create($this->_frontContent)->addClass('front'),
         Div::create($this->_backContent)->addClass('back'),
       ]
-    )
-      ->addClass('flip-container')
+    );
+    $div->addClass('flip-container')
       ->setAttribute('ontouchstart', 'this.classList.toggle(\'hover\')')
       ->setAttribute(
         'style',
         'width:' . $this->_width . ';height:' . $this->_height
       );
+    if($this->_vertical)
+    {
+      $div->addClass('vertical');
+    }
+    return $div;
   }
 }
