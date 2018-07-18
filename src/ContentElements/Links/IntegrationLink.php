916f7282ecde3675c099e3ef8b5c4fc2fc31ed17
Brooke Bryan <brooke@bajb.net>
2018-07-18 17:51:24 +0100

    Support construct

--- src/ContentElements/Links/IntegrationLink.php
+++ src/ContentElements/Links/IntegrationLink.php
@@ -7,11 +7,31 @@ class IntegrationLink extends PageletLink
 {
   protected $_baseUrl = '';
 
+  public function __construct($uri, $content = null, $selector = '#integrate-section', $baseUrl = null)
+  {
+    $this->setBaseUrl($baseUrl);
+    parent::__construct(Path::buildUnix($baseUrl, $uri), $content, $selector);
+  }
+
+  /**
+   * @param string $baseUrl
+   */
+  public function setBaseUrl($baseUrl)
+  {
+    $this->_baseUrl = $baseUrl;
+  }
+
   public static function create($uri = null, $content = null, $baseUrl = null, $selector = '#integrate-section')
   {
     $parent = new static(Path::buildUnix($baseUrl, $uri), $content, $selector);
     $parent->_baseUrl = $baseUrl;
-    $parent->setAjaxUri(Path::buildUnix($parent->_baseUrl, 'pagelet', $uri));
+    $parent->setAjaxUri($uri);
     return $parent;
+  }
+
+  public function setAjaxUri($uri)
+  {
+    $url = Path::buildUnix($this->_baseUrl, 'pagelet', $uri);
+    return parent::setAjaxUri($url);
   }
 }
