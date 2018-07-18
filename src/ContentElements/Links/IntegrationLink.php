a9ab155f62ed84d44b67a1eacfc3285d3199424d
Brooke Bryan <brooke@bajb.net>
2018-07-18 17:39:49 +0100

    Integration Link

---
+++ src/ContentElements/Links/IntegrationLink.php
@@ -0,0 +1,17 @@
+<?php
+namespace Fortifi\Ui\ContentElements\Links;
+
+use Packaged\Helpers\Path;
+
+class IntegrationLink extends PageletLink
+{
+  protected $_baseUrl = '';
+
+  public static function create($uri = null, $content = null, $baseUrl = null, $selector = '#integrate-section')
+  {
+    $parent = new static(Path::buildUnix($baseUrl, $uri), $content, $selector);
+    $parent->_baseUrl = $baseUrl;
+    $parent->setAjaxUri(Path::buildUnix($parent->_baseUrl, 'integration', $uri));
+    return $parent;
+  }
+}
